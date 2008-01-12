193fd967b60f1ce14bfbdc60c08c90f94671fb6f
Patrick van Arkel <vampier@users.sourceforge.net>
2008-01-12 07:03:08 +0000

    Made py2exe work with QT lib.
    git-svn-id: file:///openmsx-svn/code/catapult/trunk@7542 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- win32exe.py
+++ win32exe.py
@@ -1,4 +1,8 @@
+$Id: ChangeLog 1 2008-01-11 21:10:03Z Vampier $
+
+#call with python win32exe.py py2exe
+
 from distutils.core import setup
 import py2exe
 
-setup(windows=['catapult.py'])
+setup(windows=[{"script" : "catapult.py"}], options={"py2exe" : {"includes" : ["sip", "PyQt4._qt"]}})
