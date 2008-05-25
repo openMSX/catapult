d4c470df78405bcc24e2e01481bb0333ac42bb2a
Maarten ter Huurne <maarten@treewalker.org>
2008-05-25 15:32:06 +0000

    Improved layout and added some TODOs.
    git-svn-id: file:///openmsx-svn/code/catapult/trunk@7855 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/softwaredb.py
+++ src/softwaredb.py
@@ -29,50 +29,58 @@ class SoftwareDB:
 			# Connect signals.
 			#connect(ui.dirUpButton, 'clicked()', self.updir)
 
-			#fill the components with values
+			# Fill the components with values.
+			# TODO: Since Python 2.5 SQLite is part of the standard library;
+			#       please use the "sqlite3" module.
 			from pysqlite2 import dbapi2 as sqlite
 			cursor = self.__cursor
 			if cursor is None:
 				connection = sqlite.connect('softdb.db')
 				self.__cursor = cursor = connection.cursor()
 
-			#First the Compagny
+			# First the Compagny
 			ui.compagnyComboBox.addItem(QtCore.QString('Select...'))
-			cursor.execute('SELECT DISTINCT Compagny FROM software ORDER BY Compagny')
+			cursor.execute(
+				'SELECT DISTINCT Compagny FROM software ORDER BY Compagny'
+				)
 			for row in cursor:
 				ui.compagnyComboBox.addItem(QtCore.QString(row[0]))
 
-			#The Genre
+			# The Genre
 			ui.genreComboBox.addItem(QtCore.QString('Select...'))
 			cursor.execute('SELECT DISTINCT Genre FROM software ORDER BY Genre')
 			for row in cursor:
 				ui.genreComboBox.addItem(QtCore.QString(row[0]))
 
-			#The Machine
+			# The Machine
 			ui.machineComboBox.addItem(QtCore.QString('Select...'))
-			cursor.execute('SELECT DISTINCT Machine FROM software ORDER BY Machine')
+			cursor.execute(
+				'SELECT DISTINCT Machine FROM software ORDER BY Machine'
+				)
 			for row in cursor:
 				ui.machineComboBox.addItem(QtCore.QString(row[0]))
 
-			#The Patched
+			# The Patched
 			ui.patchedComboBox.addItem(QtCore.QString('Select...'))
-			cursor.execute('SELECT DISTINCT Patched FROM software ORDER BY Patched')
+			cursor.execute(
+				'SELECT DISTINCT Patched FROM software ORDER BY Patched'
+				)
 			for row in cursor:
 				ui.patchedComboBox.addItem(QtCore.QString(row[0]))
 
-			#The Type
+			# The Type
 			ui.typeComboBox.addItem(QtCore.QString('Select...'))
 			cursor.execute('SELECT DISTINCT Type FROM software ORDER BY Type')
 			for row in cursor:
 				ui.typeComboBox.addItem(QtCore.QString(row[0]))
 
-			#The Year
+			# The Year
 			ui.yearComboBox.addItem(QtCore.QString('Select...'))
 			cursor.execute('SELECT DISTINCT Year FROM software ORDER BY Year')
 			for row in cursor:
 				ui.yearComboBox.addItem(QtCore.QString(row[0]))
 
-			#Connect all dropdown boxes to the update counter method
+			# Connect all dropdown boxes to the update counter method.
 			for combox in (
 				ui.compagnyComboBox,
 				ui.genreComboBox,
@@ -138,13 +146,14 @@ class SoftwareDB:
 			print row
 			self.__ui.numbermatchesLabel.setText(str(row[0]))
 
-
 	def constructFromPartQuery(self):
 		ui = self.__ui
 		#construct the query
 		query = 'FROM software'
 		where = []
 		for gui, sqlstatement in (
+			# TODO: The proper English word is "company" (no "g").
+			#       turbor: Please fix that both in the source and in your DB.
 			(ui.compagnyComboBox, 'Compagny'),
 			(ui.genreComboBox, 'Genre'),
 			(ui.machineComboBox, 'Machine'),
@@ -153,8 +162,8 @@ class SoftwareDB:
 			(ui.yearComboBox, 'Year')
 			):
 			if gui.currentIndex() != 0:
-				where.append( str(sqlstatement) + " = '" + str(gui.currentText() ) + "'" )
-
+				where.append( str(sqlstatement) + " = '"
+					+ str(gui.currentText() ) + "'" )
 
 		for gui, sqlstatement in (
 			(ui.nameinfoLabel, 'Info'),
@@ -170,15 +179,15 @@ class SoftwareDB:
 
 	# Slots:
 
-	@QtCore.pyqtSignature("")
+	@QtCore.pyqtSignature('')
 	def on_previousPushButton_clicked(self):
 		self.__ui.stackedPages.setCurrentIndex(0)
 
-	@QtCore.pyqtSignature("")
+	@QtCore.pyqtSignature('')
 	def on_previousPushButton_2_clicked(self):
 		self.__ui.stackedPages.setCurrentIndex(1)
 
-	@QtCore.pyqtSignature("")
+	@QtCore.pyqtSignature('')
 	def on_nextPushButton_clicked(self):
 		self.__ui.stackedPages.setCurrentIndex(1)
 		query = 'SELECT * ' + self.constructFromPartQuery() + ' ORDER BY Info'
@@ -190,7 +199,9 @@ class SoftwareDB:
 			print row
 			self.__ui.gamelistView.setRowCount(index + 1)
 			founditem = QtGui.QTableWidgetItem(QtCore.QString(row[8])) # 'Info'
-			founditem.setFlags(QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsSelectable)
+			founditem.setFlags(
+				QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+				)
 			self.__ui.gamelistView.setItem(index, 0, founditem)
 			#item = QtGui.QTableWidgetItem(row[0]) # 'id'
 			#item.setFlags(QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsSelectable)
@@ -203,31 +214,30 @@ class SoftwareDB:
 		self.__ui.gamelistView.setRowCount(index + 1)
 		self.showGameinfo( self.__selectedgameid[0] )
 
-	@QtCore.pyqtSignature("")
+	@QtCore.pyqtSignature('')
 	def on_nextPushButton_2_clicked(self):
 		ui = self.__ui
 		ui.stackedPages.setCurrentIndex(2)
 		ui.gamenameLabel.setText(ui.label_name.text())
 		for gui in (
-			ui.diskaLabel ,
-			ui.diskbLabel ,
-			ui.cartaLabel ,
-			ui.cartbLabel ,
-			ui.machineLabel ,
+			ui.diskaLabel,
+			ui.diskbLabel,
+			ui.cartaLabel,
+			ui.cartbLabel,
+			ui.machineLabel,
 			ui.extensionsLabel
 			):
 			gui.setText('empty')
 
 		for gui in (
-			ui.diskaCheckBox ,
-			ui.diskbCheckBox ,
-			ui.cartaCheckBox ,
-			ui.cartbCheckBox ,
-			ui.machineCheckBox ,
+			ui.diskaCheckBox,
+			ui.diskbCheckBox,
+			ui.cartaCheckBox,
+			ui.cartbCheckBox,
+			ui.machineCheckBox,
 			ui.extensionsCheckBox
 			):
 			gui.setEnabled(True)
-
 
 		query = 'SELECT * FROM software WHERE id=' + \
 			str(self.__selectedgameid[ui.gamelistView.currentRow()]) + \
@@ -240,31 +250,29 @@ class SoftwareDB:
 			ui.machineLabel.setText(row[6])
 			ui.diskaLabel.setText(row[9])
 
-
-	
-	@QtCore.pyqtSignature("")
+	@QtCore.pyqtSignature('')
 	def on_applyPushButton_clicked(self):
 		ui = self.__ui
-		#First of all change machine if needed
+		# First of all change machine if needed.
 		if ui.machineCheckBox.isChecked():
 			# Request machine change from openMSX.
-			# TODO skip changing if openMSX is running with the correct machine
+			# TODO: Skip change if openMSX is running with the correct machine.
 			self.__bridge.command('machine', ui.machineLabel.text())(
 				self.__applyMedia,
 				self.__machineChangeErrorHandler
 				)
 
-		# Then the needed extension
-			#TODO implement this :-)
+		# Then the needed extension.
+		# TODO: Implement this :-)
 
 	def __applyMedia(self):
 		# Insert(/eject) the media if requested
 		ui = self.__ui
 		for check, label, media in (
-			( ui.diskaCheckBox, ui.diskaLabel, 'diska'),
-			( ui.diskbCheckBox, ui.diskbLabel, 'diskb'),
-			( ui.cartaCheckBox, ui.cartaLabel, 'carta'),
-			( ui.cartbCheckBox, ui.cartbLabel, 'cartb')
+			( ui.diskaCheckBox, ui.diskaLabel, 'diska' ),
+			( ui.diskbCheckBox, ui.diskbLabel, 'diskb' ),
+			( ui.cartaCheckBox, ui.cartaLabel, 'carta' ),
+			( ui.cartbCheckBox, ui.cartbLabel, 'cartb' )
 			):
 			print "xxxxxxxxxxxxxxxx" + str(media)
 			if check.isChecked():
@@ -277,19 +285,17 @@ class SoftwareDB:
 					print "zzzzzzzzzzzzzzzzz " + str(label.text())
 			else:
 				print "yyyyyyyyyyyyyyyyy not checked"
-		# Do we actually want to close this dialog once a software is chosen ?
+		# TODO: Do we actually want to close this dialog once a software is
+		#       chosen?
 		self.__dmDialog.hide()
 
-
 	def __machineChangeErrorHandler(self, message):
-		messageBox =  QtGui.QMessageBox('Problem changing machine:', message,
-				QtGui.QMessageBox.Warning, 0, 0, 0,
-				self.__dmDialog
-				)
+		messageBox = QtGui.QMessageBox(
+			'Problem changing machine:', message,
+			QtGui.QMessageBox.Warning, 0, 0, 0,
+			self.__dmDialog
+			)
 		messageBox.show()
-
-
-
 
 	def showGameinfo( self , softid ):
 		query = "SELECT * FROM software WHERE id = '" + str(softid) + "'"
@@ -299,15 +305,14 @@ class SoftwareDB:
 		for row in cursor:
 			#print row
 			#info on page 2
-			self.__ui.label_name.setText( QtCore.QString( row[8] ))
-			self.__ui.label_compagny.setText( QtCore.QString( row[4] ))
-			self.__ui.label_year.setText( QtCore.QString( row[2] ))
-			self.__ui.label_machine.setText( QtCore.QString( row[6] ))
-			self.__ui.label_genre.setText( QtCore.QString( row[7] ))
+			self.__ui.label_name.setText(QtCore.QString(row[8]))
+			self.__ui.label_compagny.setText(QtCore.QString(row[4]))
+			self.__ui.label_year.setText(QtCore.QString(row[2]))
+			self.__ui.label_machine.setText(QtCore.QString(row[6]))
+			self.__ui.label_genre.setText(QtCore.QString(row[7]))
 
-
-	def gamelistView_cellClicked( self, row, dummy ):
-		self.showGameinfo( self.__selectedgameid[row] )
+	def gamelistView_cellClicked(self, row, column): # pylint: disable-msg=W0613
+		self.showGameinfo(self.__selectedgameid[row])
 
 	#def __updateAll(self):
 		# TODO: The idea of the name "updateAll" was to be able to deal with
