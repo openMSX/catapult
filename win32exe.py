007672d54f5deb81759554bf0491001ae455314a
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2008-09-21 11:27:02 +0000

    Fixed for new directory structure and optimized some options and updated it for the latest libraries (see first few lines of the script). Also made it more readable.
    git-svn-id: file:///openmsx-svn/code/catapult/trunk@8254 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- win32exe.py
+++ win32exe.py
@@ -1,7 +1,29 @@
 # $Id$
-#call with python win32exe.py py2exe
+# call with:
+# PYTHONPATH=derived python win32exe.py py2exe
 
 from distutils.core import setup
 import py2exe
+import glob
 
-setup(windows=[{"script" : "catapult.py"}], options={"py2exe" : {"includes" : ["sip", "PyQt4._qt"]}})
+setup(
+	windows = [
+		{
+			"script" : "derived/catapult.py",
+			"icon_resources": [(0, "derived/catapult.ico")]
+		}
+	],
+	data_files = [
+			("", glob.glob("derived/*.png"))
+		],
+	zipfile = None,
+	options = {"py2exe" : {
+			"includes" : ["sip", "PyQt4"],
+			"dist_dir" : "derived/dist",
+                	"unbuffered": True,
+			"optimize": 2,
+			"bundle_files": 1,
+			"compressed": 1
+			}
+		}
+	)
