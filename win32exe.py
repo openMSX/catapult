e6c28dbc81232bcf41db1e27731fb3fd184c8a61
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2008-09-21 12:37:27 +0000

    Fill in windows version info resources.
    git-svn-id: file:///openmsx-svn/code/catapult/trunk@8255 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- win32exe.py
+++ win32exe.py
@@ -9,17 +9,24 @@ import glob
 setup(
 	windows = [
 		{
-			"script" : "derived/catapult.py",
-			"icon_resources": [(0, "derived/catapult.ico")]
+			"script": "derived/catapult.py",
+			"icon_resources": [(0, "derived/catapult.ico")],
+			"name": "openMSX Catapult",
+			"version": "0.0.0",
+			"description": "The GUI for openMSX",
+			"author": "openMSX Team",
+			"copyright": "(c) 2006-2008, openMSX Team",
+			"comments": "beware that this is still a prototype!",
+			"company_name": "openMSX Team"
 		}
 	],
 	data_files = [
 			("", glob.glob("derived/*.png"))
 		],
 	zipfile = None,
-	options = {"py2exe" : {
-			"includes" : ["sip", "PyQt4"],
-			"dist_dir" : "derived/dist",
+	options = {"py2exe": {
+			"includes": ["sip", "PyQt4"],
+			"dist_dir": "derived/dist",
                 	"unbuffered": True,
 			"optimize": 2,
 			"bundle_files": 1,
